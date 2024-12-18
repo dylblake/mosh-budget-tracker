0931eb1557ee7af39fcbeb3ae0da660e8acaa545
Dylan Evans <dylbalke03@gmail.com>
2024-12-18 17:21:10 -0500

    Add titles and submit button

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -18,41 +18,57 @@ const Form = () => {
     console.log(data);
   };
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="mb-3">
-        <label htmlFor="description" className="form-label">
-          Description
-        </label>
-        <input
-          {...register("description")}
-          id="description"
-          type="text"
-          className="form-control"
-        />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="amount" className="form-label">
-          Amount
-        </label>
-        <input
-          {...register("amount")}
-          id="amount"
-          type="number"
-          className="form-control"
-        />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="category" className="form-label">
-          Category
-        </label>
-        <select {...register("category")} id="category" className="form-select">
-          <option value="" />
-          <option value="groceries">Groceries</option>
-          <option value="utilities">Utilities</option>
-          <option value="entertainment">Entertainment</option>
-        </select>
-      </div>
-    </form>
+    <>
+      <h1>Budget Tracker</h1>
+      <hr />
+      <h4>Use this form to submit new expenses.</h4>
+      <hr />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-3">
+          <label htmlFor="description" className="form-label">
+            Description
+          </label>
+          <input
+            {...register("description")}
+            id="description"
+            type="text"
+            className="form-control"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="amount" className="form-label">
+            Amount
+          </label>
+          <input
+            {...register("amount")}
+            id="amount"
+            type="number"
+            className="form-control"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="category" className="form-label">
+            Category
+          </label>
+          <select
+            {...register("category")}
+            id="category"
+            className="form-select"
+          >
+            <option value="" />
+            <option value="groceries">Groceries</option>
+            <option value="utilities">Utilities</option>
+            <option value="entertainment">Entertainment</option>
+          </select>
+        </div>
+        <button className="btn btn-primary" type="submit">
+          Submit
+        </button>
+      </form>
+      <hr />
+      <h4>Filter the table and remove expenses below.</h4>
+      <hr />
+    </>
   );
 };
 
